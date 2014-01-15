d4dd803bc934471b68db24ece53e5e7d19f42f67
gnishida <gnishida@purdue.edu>
2014-01-15 16:41:13 -0500

    Update.

---
+++ RoadSimplification/ThinningUtil.h
@@ -0,0 +1,25 @@
+﻿/**
+ * This thinning utility implements the following website code.
+ * https://opencv-code.com/quick-tips/implementation-of-guo-hall-thinning-algorithm/#more-675
+ * 元になる論文は、以下。
+ * Z. Guo and R. Hall. Parallel thinning with two-subiteration algorithms. Communications of the ACM, 32(3):359-373, 1989.
+ *
+ */
+
+#pragma once
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+class ThinningUtil {
+protected:
+	ThinningUtil() {}
+	~ThinningUtil() {}
+
+private:
+	static void thinningGuoHallIteration(cv::Mat& im, int iter);
+
+public:
+	static void thinningGuoHall(cv::Mat& im);
+};
+
--- RoadSimplification/main.cpp
+++ RoadSimplification/main.cpp
@@ -10,45 +10,44 @@
 #include <iostream>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
-#include <QVector2D>
-#include <QHash>
 #include "GraphUtil.h"
-#include "BBox.h"
+#include "ThinningUtil.h"
 
 int main(int argc, char *argv[]) {
-	int size = 10;
-	float range[] = { 0, 300 };
-	const float* ranges = { range };
+	if (argc < 2) {
+		std::cerr << "Usage: RoadSimplification <road GSM file> <road type>" << std::endl;
+		return 1;
+	}
+	
+	cv::Mat result;
 
-	// create a histogram
-	cv::Mat data1 = (cv::Mat_<float>(1, 10) << 20, 30, 30, 40, 80, 90, 120, 120, 130, 150);
-	cv::MatND hist1;
-	cv::calcHist(&data1, 1, 0, cv::Mat(), hist1, 1, &size, &ranges, true, false);
+	if (argc == 3) {
+		int roadType = atoi(argv[2]);
 
-	// create a histogram
-	cv::Mat data2 = (cv::Mat_<float>(1, 10) << 30, 33, 40, 70, 90, 100, 130, 135, 140, 154);
-	cv::MatND hist2;
-	cv::calcHist(&data2, 1, 0, cv::Mat(), hist2, 1, &size, &ranges, true, false);
+		// AvenueとLocal streetsのみを読み込む
+		RoadGraph r;
+		GraphUtil::loadRoads(r, argv[1], roadType);
 
-	// create a histogram
-	cv::Mat data3 = (cv::Mat_<float>(1, 10) << 30, 83, 140, 170, 190, 200, 200, 205, 240, 244);
-	cv::MatND hist3;
-	cv::calcHist(&data3, 1, 0, cv::Mat(), hist3, 1, &size, &ranges, true, false);
+		// Create a matrix from the road object
+		cv::Mat_<uchar> mat;
+		GraphUtil::convertToMat(r, mat, cv::Size(1000, 1000), QVector2D(500, 500));
 
-	// compute the similarity
-	float similarity = cv::compareHist(hist1, hist2, 0);
-	printf("data1 - data2: %lf\n", similarity);
-	similarity = cv::compareHist(hist2, hist3, 0);
-	printf("data2 - data3: %lf\n", similarity);
-	similarity = cv::compareHist(hist3, hist1, 0);
-	printf("data3 - data1: %lf\n", similarity);
+		// Dilation
+		cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(21, 21));//, cv::Point(10, 10));
+		cv::dilate(mat, result, kernel);
+
+		// Thinning
+		ThinningUtil::thinningGuoHall(result);
+	} else {
+		result = cv::imread(argv[1], 0);
+
+		// Thinning
+		ThinningUtil::thinningGuoHall(result);
+	}
 
 	// Display
-	/*
-	cv::flip(result, result, 1);
-	cv::namedWindow("segmentation", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
-	cv::imshow("segmentation", result * 64);
-	*/
+	cv::namedWindow("result", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
+	cv::imshow("result", result);
 
 	cv::waitKey(0);
 }
